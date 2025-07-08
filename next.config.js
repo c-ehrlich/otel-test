3bf17438b8ca5d20c8f2b9c716b3552016d16e2b
c-ehrlich <ehrlich.christopher@gmail.com>
2025-07-08 14:58:33 +0700

    fix r-i-t-m issue

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Externalize problematic packages to avoid bundling issues
+      config.externals = config.externals || [];
+      config.externals.push('require-in-the-middle');
+      config.externals.push('handlebars');
+      config.externals.push('nunjucks');
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
