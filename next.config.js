b1ec948dd8386a6fe4b377d844929cd4faad285c
c-ehrlich <ehrlich.christopher@gmail.com>
2025-07-08 15:48:49 +0700

    forgot to commit this

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfg = {
   webpack: (config, { isServer }) => {
     if (isServer) {
       // Externalize problematic packages to avoid bundling issues
       config.externals = config.externals || [];
       config.externals.push('require-in-the-middle');
-      config.externals.push('handlebars');
-      config.externals.push('nunjucks');
     }
     return config;
   },
